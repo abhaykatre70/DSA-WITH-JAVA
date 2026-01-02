1f838468781a781c28fcd0beae8524a8638e756e
Abhay Katre <abhaykatre70@gmail.com>
2026-01-02 23:57:20 +0530

    Butterfly Star Pattern

--- revision.java
+++ revision.java
@@ -207,21 +207,80 @@ public class revision {
         // }
 
         //Palindrome Triangle Pattern
-        int n=6;
-        for (int i = 1; i <= n; i++) {
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for(int j=i;j>=1;j--){
-                System.out.print(j);
-            }
-            for(int j=2;j<=i;j++){
-                System.out.print(j);
-            }
-            System.out.println();       
-        }
+        // int n=6;
+        // for (int i = 1; i <= n; i++) {
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+        //     for(int j=2;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();       
+        // }
+
+        // Rhombus pattern
+        // int n=6;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0;j<n;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Diamond Star Pattern
+        // int n=6;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n-i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <=2*i-1 ; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = n-1; i >=1; i--) {
+        //     for (int j = 1; j <= n-i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <=2*i-1 ; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
         
-    }
+    //     //Butterfly Star Pattern
+    //     int n=6;
+    //     for (int i = 1; i <=n; i++) {
+    //         for (int j = 1; j <=i; j++) {
+    //             System.out.print("*");
+    //         }
+    //         for (int j = 1;j<=2*n-2*i; j++) {
+    //             System.out.print(" ");
+    //         }
+    //         for (int j = 1; j <=i; j++) {
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    //     for (int i = n; i >=1; i--) {
+    //         for (int j = 1; j <=i; j++) {
+    //             System.out.print("*");
+    //         }
+    //         for (int j = 1;j<=2*n-2*i; j++) {
+    //             System.out.print(" ");
+    //         }
+    //         for (int j = 1; j <=i; j++) {
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 
     // static boolean isArmstrong(int num) {
     //     int original = num;
